5acb9acb0f59fbc87d12c97b7e02fe13c02ba12d
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-18 23:53:11 -0300

    feat: config first db connection

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,27 @@
-import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import z from 'zod'
+
+
 
 const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.post('/polls', async (req:FastifyRequest, res:FastifyReply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  })
+
+  const {title} = createPollBody.parse(req.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+  return res.status(201).send({pollId: poll.id})
+})
 
 app.listen({
   port: 3333,
