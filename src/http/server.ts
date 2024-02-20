6ceb4e67c4dc6084027e79f04b5ad6b460c1a5a4
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-19 21:51:28 -0300

    feat: register get route poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,11 @@
 
 import fastify from 'fastify'
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
 
 const app = fastify()
 
-app.register(createPoll)
+app.register(createPoll, getPoll)
 app.listen({
   port: 3333,
 }).then(() => {
